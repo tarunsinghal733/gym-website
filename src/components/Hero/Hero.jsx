1830300622de655045a7acb7df71cb53416bff67
tarunsinghal733 <20BCS3552cuchd.in>
2023-11-25 20:50:32 +0530

    vbn

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../Hero/Hero.css"
+
+const Hero = () => {
+    return (
+        <div>
+            <div className='hero'>
+                <div className='hero-l'>
+
+                </div>
+                <div className='hero-r'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
